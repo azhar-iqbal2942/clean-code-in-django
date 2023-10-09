6bee261e1faf30b75c238f60ef472a629ca129a9
Azhar Iqbal <azhariqbal2942@gmail.com>
2023-10-09 13:18:24 +0500

    Remove extra imports

--- src/api/rest/v1/room.py
+++ src/api/rest/v1/room.py
@@ -1,7 +1,5 @@
 from rest_framework.views import APIView
 from rest_framework.permissions import AllowAny
-from rest_framework.response import Response
-from rest_framework import status
 
 from handlers.room_handler import RoomHandler
 
